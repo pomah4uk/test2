067a84650e22fa13336e55eaef3abfeee1af69f6
Andrew <code70@inbox.ru>
2023-02-06 15:22:01 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+	<div class="container-fluid py-5 bg-dark">
+		<div class="row align-items-center justify-content-center">
+			<div class="col-12 col-md-10 col-lg-10">
+				<h1 class="text-white">Header</h1>
+			</div>
+		</div>
+	</div>
+<script src="assets/main.js"></script>
+</body>
+</html>
